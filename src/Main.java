6b2bb1434bf4fb5e618662e17692613d7135d704
Karl Nyhlin <Kalle@Karls-MacBook-Pro.local>
2023-10-07 15:10:02 +0200

    created Board and Player classes

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        char[][] board = {
-                {' ', '|', ' ', '|', ' '},
-                {'–', '+', '–', '+', '–'},
-                {' ', '|', ' ', '|', ' '},
-                {'–', '+', '–', '+', '–'},
-                {' ', '|', ' ', '|', ' '}};
+        Board board = new Board();
+        Board.printBoard(Board.board);
 
-        printBoard(board);
+        /*
+        TODO:
+        Skapa en tom spelplan "board" i en klass. Spelplanen är en 2d-array av char.
+        Klassen Board ska ha en metod printBoard som skriver ut spelplanen.
+        Ta input från spelaren om spelarnas namn.
+        Skapa en klass Player som har en instansvariablerna name och symbol (om de är X eller O).
+        Starta en spelloop, håll koll på vems tur det är (instansvariabel i Player?),
+        låt varje spelare i tur och ordning ange ett nummer 1-9 för var de vill placera sin symbol.
+        Kolla att fältet är tomt. Om det är det, skriv ut spelplanen med den tillagda symbolen.
+        Skapa en funktion som kollar om någon vinner. Kolla sedan om det i nästa drag blir oavgjort.
 
-
-        //ArrayList<ArrayList<String>> board = new ArrayList<>(3);
-        //ArrayList<String> row1 = new ArrayList<>(3);
-        //String row1col1 = "┌─────\n|     ";
-        //String row1col2 = "┌─────\n|     ";
-        //String row1col3 = "┬─────┐\n|     |";
-        //ArrayList<String> row2 = new ArrayList<>(3);
-        //ArrayList<String> row3 = new ArrayList<>(3);
-        ////String str1 = "┌─────┬─────┬─────┐\n" +
-        ////       "                |     │  X  |     |";
-        //row1.add(0, row1col1);
-        //row1.add(1, row1col2);
-        //row1.add(2, row1col3);
-        //board.add(0, row1);
-        //board.add(1, row2);
-        //board.add(2, row3);
-        //System.out.println(board.get(0).);
-
-        //String board = Arrays.toString(new String[3][3]);
-       // String[][] board = new String[][]{
-       //         {"┌─────\n|     \n|-----", "┌─────\n|     \n|-----", "┌─────\n|     \n|-----"},
-       //         {"┌─────\n|     \n|-----", "┌─────\n|     \n|-----", "┌─────\n|     \n|-----"},
-       //         {"┌─────\n|     \n|-----", "┌─────\n|     \n|-----", "┌─────\n|     \n|-----"}
-       // };
-       // // print the board
-       // for (int i = 0; i < 3; i++) {
-       //     for (int j = 0; j < 3; j++) {
-       //         System.out.print(board[i][j]);
-       //     }
-       // }
-        //System.out.println(Arrays.deepToString(board));
-        /*System.out.println("""
-                ┌─────┬─────┬─────┐
-                |     │  X  |     |
-                |-----|-----|-----|
-                |     |  O  |     |
-                |-----|-----|-----|
-                |     |     |     |
-                |-----|-----|-----|
-                
-                """);
+         */
 
 
-    }*/
+        // Box-drawing characters:
+        //System.out.println("\u2500 \u2502 \u250C \u2510 \u2514 \u2518 \u251C \u2524 \u252C " +
+        //        "\u2534 \u253C \u2550 \u2551 \u2552 \u2553 \u2554 \u2555 \u2556\u2557 \u2558 " +
+        //        "\u2559 \u255A \u255B \u255C \u255D \u255E \u255F\u2560 \u2561 \u2562 \u2563 " +
+        //        "\u2564 \u2565 \u2566 \u2567 \u2568 \u2569 \u256A \u256B \u256C");
 
-        //char[][] board = new char[3][3];
-        //for (int i = 0; i < 3; i++) {
-        //    for (int j = 0; j < 3; j++) {
-        //        board[i][j] = '-';
-        //    }
-        //}
-        //for (int i = 0; i < 3; i++){
-        //    System.out.print(board.get(i));
-        //    for (int j = 0; j < 3; j++) {
-        //        System.out.println(board.get(j));
-        //    }
-        //}
-        //System.out.println(Arrays.deepToString(board));
 
 
     }
@@ -80,12 +35,5 @@ public class Main {
 
 
 
-public static void printBoard(char[][] board) {
-    for (char[] row : board) {
-        for (char c : row) {
-            System.out.print(c);
-        }
-        System.out.println();
-    }
-}
+
 }
